bd31a74d94eb205d69ca3e297c332e0021ff2690
Lada303 <afanaseva.lada@gmai.com>
2022-01-20 14:27:39 +0300

    Protocol: connected the protocol module with service commands

---
+++ protocol/src/main/java/Lada303/service/ServiceCommands.java
@@ -0,0 +1,13 @@
+package Lada303.service;
+
+public class ServiceCommands {
+    public static final String AUTH = "/auth";
+    public static final String AUTH_OK = "/authOk";
+    public static final String REG = "/reg";
+    public static final String REG_OK = "/regOk";
+    public static final String REG_NO = "/regNo";
+    public static final String CLIENTS = "/clients";
+    public static final String END = "/end";
+    public static final String W = "/w";
+
+}
